5e9d5276d14ed42682c41ebe046e8beeb65df73f
Maximiliano <mvillasmil00@gmail.com>
2023-08-19 19:23:12 -0400

    adjusted main container padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Stopwatch from "./timer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-mono">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-5 font-mono">
       <Stopwatch />
     </main>
   );
