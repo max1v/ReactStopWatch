ec100391f25292d280071ea08776f3763e687e57
Maximiliano <mvillasmil00@gmail.com>
2023-08-26 07:38:30 -0400

    Updated entire UI using shadcn ui

--- app/timer.tsx
+++ app/timer.tsx
@@ -2,14 +2,33 @@
 
 import { useState } from "react";
 
-import { useStopWatch } from "./components/stopwatchhook";
+import {
+  useStopWatch,
+  type TimerOptions,
+  type PreparationOptions,
+} from "./components/stopwatchhook";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Switch } from "@/components/ui/switch";
 
 export default function Stopwatch() {
   const [timerOptions, setTimerOptions] = useState({
     targetTimeEnabled: false,
-    targetTime: "0",
+    targetTime: 10,
     stopAtTargetTime: false,
-    savelimit: 5,
   });
   const [preparationOptions, setPreparationOptions] = useState({
     preparationEnabled: false,
@@ -36,204 +55,191 @@ export default function Stopwatch() {
   }
 
   return (
-    <div className="flex flex-col gap-5 sm:flex-row sm:items-start">
-      {
-        <OptionsPanel
-          options={timerOptions}
-          setOptions={setTimerOptions}
-          preparationOptions={preparationOptions}
-          setPreparationOptions={setPreparationOptions}
-        />
-      }
-      <div className="flex min-w-max flex-col items-center justify-between gap-5 rounded-[5px] border-2 border-slate-900 p-5">
-        <h1 className="text-center text-xl">Hold Stopwatch</h1>
-        <div>
-          <p>{time.seconds} seconds</p>
-          <p>{time.minutes} minutes</p>
-          <p>{time.hours} hours</p>
-        </div>
-        <p>
-          {status} {reachedTarget && "Target Reached"}
-          {status === "preparing" && `${prepTime}s`}
-        </p>
-        <div id="controls" className="flex justify-between gap-1">
-          {started ? (
-            <button
-              ref={buttonref}
-              className="bg-slate-800 px-5 py-2 text-xl text-white"
-              onClick={handleStartStopButtons}
-            >
-              Stop
-            </button>
-          ) : (
-            <button
-              ref={buttonref}
-              className="bg-slate-800 px-5 py-2 text-xl text-white"
-              onClick={handleStartStopButtons}
-            >
-              Start
-            </button>
-          )}
-        </div>
-        <ul>
-          <h1>Previous Times</h1>
-          {previousTimes
-            .map((t) => {
-              return (
-                <li key={t.id}>{`${t.seconds}s ${t.minutes}m ${t.hours}h`}</li>
-              );
-            })
-            .reverse()}
-        </ul>
-        <p>Does it work?</p>
-      </div>
+    <div className="flex flex-col gap-5">
+      <Card>
+        <Tabs defaultValue="timer" className="flex flex-col items-center p-5">
+          <TabsList>
+            <TabsTrigger value="timer">Timers</TabsTrigger>
+            <TabsTrigger value="settings">Settings</TabsTrigger>
+          </TabsList>
+          <TabsContent value="timer">
+            <div className="flex  flex-col items-center justify-between gap-5 text-center">
+              <CardHeader>
+                <CardTitle className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                  Hold Stopwatch
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col items-center gap-5">
+                <div className="flex flex-col items-start">
+                  <p className="leading-7">{time.seconds} seconds</p>
+                  <p className="leading-7">{time.minutes} minutes</p>
+                  <p className="leading-7">{time.hours} hours</p>
+                  {timerOptions.targetTimeEnabled && (
+                    <p className="text-left text-sm text-muted-foreground">{`Target time set at ${
+                      timerOptions.targetTime
+                    }s ${
+                      timerOptions.stopAtTargetTime
+                        ? " and will stop when it reaches the target"
+                        : ""
+                    }`}</p>
+                  )}
+                  {preparationOptions.preparationEnabled && (
+                    <p className="text-left text-sm text-muted-foreground">{`Preparation set to last ${preparationOptions.preparationTime}s`}</p>
+                  )}
+                </div>
+                <Badge
+                  variant={`${status === "stopped" ? "outline" : "default"}`}
+                >
+                  {status}
+                  {status === "preparing" && `${prepTime}s`}
+                </Badge>
+                {reachedTarget && (
+                  <Badge variant="secondary">Target time reached</Badge>
+                )}
+                <Separator />
+                <Button
+                  ref={buttonref}
+                  className=" px-5 py-2 text-xl text-white"
+                  variant={started ? "destructive" : "default"}
+                  onClick={handleStartStopButtons}
+                >
+                  {started ? "Stop" : "Start"}
+                </Button>
+                <Separator />
+                {previousTimes.length > 0 && (
+                  <ul>
+                    <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                      Previous Times
+                    </h2>
+                    <ScrollArea className="h-[200px] rounded-md border p-4">
+                      {previousTimes
+                        .map((t) => {
+                          return (
+                            <li
+                              key={t.id}
+                            >{`${t.seconds}s ${t.minutes}m ${t.hours}h`}</li>
+                          );
+                        })
+                        .reverse()}
+                    </ScrollArea>
+                  </ul>
+                )}
+              </CardContent>
+            </div>
+          </TabsContent>
+          <TabsContent value="settings">
+            <OptionsPanel
+              options={timerOptions}
+              setOptions={setTimerOptions}
+              preparationOptions={preparationOptions}
+              setPreparationOptions={setPreparationOptions}
+            />
+          </TabsContent>
+        </Tabs>
+      </Card>
     </div>
   );
 }
+
+type OptionsPanelProps = {
+  options: TimerOptions;
+  setOptions: Function;
+  preparationOptions: PreparationOptions;
+  setPreparationOptions: Function;
+};
 
 function OptionsPanel({
   options,
   setOptions,
   preparationOptions,
   setPreparationOptions,
-}: {
-  options: {
-    targetTimeEnabled: boolean;
-    targetTime: string;
-    stopAtTargetTime: boolean;
-  };
-  setOptions: Function;
-  preparationOptions: {
-    preparationEnabled: boolean;
-    preparationTime: number;
-  };
-  setPreparationOptions: Function;
-}) {
-  const [togglePanel, setTogglePanel] = useState(false);
-
-  if (!togglePanel)
-    return (
-      <div className="flex flex-col justify-center rounded-[5px] border-2 border-slate-900 p-5">
-        <button
-          className="rounded-[5px] border-2 border-slate-900 bg-white text-center"
-          onClick={() => setTogglePanel(!togglePanel)}
-        >
-          Options Panel
-        </button>
-      </div>
-    );
+}: OptionsPanelProps) {
   return (
-    <div className="flex flex-col justify-center rounded-[5px] border-2 border-slate-900 p-5">
-      <button
-        className="rounded-[5px] border-2 border-slate-900 bg-white text-center"
-        onClick={() => setTogglePanel(!togglePanel)}
-      >
+    <div className="flex flex-col justify-center p-5">
+      <CardTitle className="scroll-m-20 text-center text-2xl font-semibold tracking-tight">
         Options Panel
-      </button>
-      <form className="flex flex-col">
+      </CardTitle>
+      <form className="mt-5 flex flex-col items-center gap-2">
         <div className="flex items-center gap-2 p-2">
-          <label htmlFor="targetTimeToggle">Enable Target Time</label>
-          <input
-            type="checkbox"
+          <Label
+            htmlFor="targetTimeToggle"
+            className="scroll-m-20 text-xl font-semibold tracking-tight"
+          >
+            Enable target time
+          </Label>
+          <Switch
             id="targetTimeToggle"
             checked={options.targetTimeEnabled}
-            onChange={(e) =>
-              setOptions({ ...options, targetTimeEnabled: e.target.checked })
+            onCheckedChange={(checked) =>
+              setOptions({ ...options, targetTimeEnabled: checked })
             }
           />
         </div>
         {options.targetTimeEnabled && (
-          <div className="flex flex-col justify-center gap-2 p-2">
-            <label htmlFor="selectTargetTime">Target Time in seconds</label>
-            <input
-              className="text-center"
+          <Card className="flex flex-col items-center gap-2 p-5">
+            <Label htmlFor="selectTargetTime">Target Time in seconds</Label>
+            <Input
               type="number"
-              min="0"
-              step="1"
+              min="1"
               autoComplete="off"
               value={options.targetTime}
               id="selectTargetTime"
               onChange={(e) => {
                 setOptions({ ...options, targetTime: e.target.value });
               }}
-            ></input>
-            <label htmlFor="stopattargetcheck">Stop at target time</label>
-            <input
-              type="checkbox"
+            />
+            <Label htmlFor="stopattargetcheck">Stop at target time</Label>
+            <Switch
               id="stopattargetcheck"
               checked={options.stopAtTargetTime}
-              onChange={(e) =>
-                setOptions({ ...options, stopAtTargetTime: e.target.checked })
+              onCheckedChange={(c) =>
+                setOptions({ ...options, stopAtTargetTime: c })
               }
             />
-          </div>
+          </Card>
         )}
+        <Separator />
         <div className="flex items-center gap-2 p-2">
-          <label htmlFor="preparationTimeToggle">Enable Preparation Time</label>
-          <input
-            type="checkbox"
+          <Label
+            htmlFor="preparationTimeToggle"
+            className="scroll-m-20 text-xl font-semibold tracking-tight"
+          >
+            Enable preparation time
+          </Label>
+          <Switch
             id="preparationTimeToggle"
             checked={preparationOptions.preparationEnabled}
-            onChange={(e) =>
+            onCheckedChange={(checked) =>
               setPreparationOptions({
                 ...preparationOptions,
-                preparationEnabled: e.target.checked,
+                preparationEnabled: checked,
               })
             }
           />
         </div>
         {preparationOptions.preparationEnabled && (
-          <div className="flex flex-col justify-center gap-2 p-2">
-            <h1 className="text-center">Select Preparation time</h1>
-            <button
-              className={`border-2 ${
-                preparationOptions.preparationTime === 5
-                  ? "border-blue-500"
-                  : "border-slate-900"
-              }`}
-              onClick={(e) => {
-                e.preventDefault();
+          <Card className="flex flex-col items-center gap-2 p-5">
+            <Label>Select Preparation time</Label>
+            <Select
+              defaultValue={preparationOptions.preparationTime?.toString()}
+              onValueChange={(currentValue) => {
+                // e.preventDefault();
                 setPreparationOptions({
                   ...preparationOptions,
-                  preparationTime: 5,
+                  preparationTime: parseInt(currentValue),
                 });
               }}
             >
-              5s
-            </button>
-            <button
-              className={`border-2 ${
-                preparationOptions.preparationTime === 15
-                  ? "border-blue-500"
-                  : "border-slate-900"
-              }`}
-              onClick={(e) => {
-                e.preventDefault();
-                setPreparationOptions({
-                  ...preparationOptions,
-                  preparationTime: 15,
-                });
-              }}
-            >
-              15s
-            </button>
-            <button
-              className={`border-2 ${
-                preparationOptions.preparationTime === 30
-                  ? "border-blue-500"
-                  : "border-slate-900"
-              }`}
-              onClick={(e) => {
-                e.preventDefault();
-                setPreparationOptions({
-                  ...preparationOptions,
-                  preparationTime: 30,
-                });
-              }}
-            >
-              30s
-            </button>
-          </div>
+              <SelectTrigger>
+                <SelectValue placeholder="Time before start" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="5">5s</SelectItem>
+                <SelectItem value="15">15s</SelectItem>
+                <SelectItem value="30">30s</SelectItem>
+              </SelectContent>
+            </Select>
+          </Card>
         )}
       </form>
     </div>
